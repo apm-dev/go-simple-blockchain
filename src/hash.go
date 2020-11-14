7cc09105f804aa7bb02140eb5e9fb7b94ac51051
Amir Parsa <develop.apm@gmail.com>
2020-11-14 19:56:14 +0330

    Implement SHA256 hash method

--- src/hash.go
+++ src/hash.go
@@ -1,9 +1,34 @@
 package src
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"hash"
+)
+
 const (
 	HashSHA256 = "sha256"
 )
 
-func Hash(method string, obj interface{}) string {
-	return ""
+var h hash.Hash
+
+func Hash(method string, obj interface{}) (string, error) {
+	input, err := json.Marshal(obj)
+	if err != nil {
+		return "", err
+	}
+
+	switch method {
+	case HashSHA256:
+		h = sha256.New()
+	default:
+		return "", errors.New(fmt.Sprintf("%s hash method not exists", method))
+	}
+
+	defer h.Reset()
+	h.Write(input)
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
